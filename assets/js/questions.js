08fb1739bda711f59540b6c6d18523f20421a58e
William Cheausky <williamchenausky@yahoo.com>
2021-01-04 13:50:44 -0500

    first push for files

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        trivia: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "boolenas", "all of the above"],
+        answer: "all of the above"
+    }
+var questions = [
+    {
+        trivia: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "boolenas", "all of the above"],
+        answer: "all of the above"
+    }
+var questions = [
+    {
+        trivia: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "boolenas", "all of the above"],
+        answer: "all of the above"
+    }
+var questions = [
+    {
+        trivia: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "boolenas", "all of the above"],
+        answer: "all of the above"
+    }
+var questions = [
+    {
+        trivia: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "boolenas", "all of the above"],
+        answer: "all of the above"
+    }
+]
