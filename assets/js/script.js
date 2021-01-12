a50634abe26fc00d19393bf460503100ad00199f
William Cheausky <williamchenausky@yahoo.com>
2021-01-12 11:34:45 -0500

    add timer and hide on apge load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 var highscoreScreen = document.querySelector("#highscore")
 var timerCountdown = document.querySelector("#time");
 var startQuizBtn = document.querySelector("#start-quiz-btn")
+var quizScreen = document.querySelector("#quiz-section");
+var scoreDisplay = document.querySelector("#highscore-display")
+
+function pageLoad() {
+    highscoreScreen.setAttribute("class", "hide");
+    quizScreen.setAttribute("class", "hide");
+    scoreDisplay.setAttribute("class", "hide");
+  
+    if (startQuiz == true){
+      return;
+    }
+  
+  }
 
 function startQuiz() {
     titleScreen.setAttribute("class", "hide");
@@ -16,3 +29,7 @@ function startQuiz() {
   
     getQuestion();
   }
+
+  var currentQuestionIndex = 0;
+var time = 75;
+var timer;
