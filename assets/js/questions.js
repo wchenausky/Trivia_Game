32cad091eeb5a813bdf33e71ffc897b2e15d2c1f
William Cheausky <williamchenausky@yahoo.com>
2021-01-12 14:57:46 -0500

    css complete start getting fucntions working

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,28 +1,29 @@
+
 var questions = [
     {
-        trivia: "Arrays in JavaScript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "boolenas", "all of the above"],
-        Answer: "all of the above",
-
+      quizQuestion: "Arrays in JavaScript can be used to store _____.",
+      answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      correct:"all of the above"
     },
     {
-        trivia: "String Values must be enclosed within ____ when being assigned to variables.",
-        answers: ["commas", "curly brackets", "quotes", "parenthesis"],
-        correct:"quotes"
-      },
-      {
-        trivia: "The condition in an if/else statement is enclosed with ____.",
-        answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-        correct:"curly brackets"
-      },
-      {
-        trivia: "Commonly used data types DO NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct:"alerts"
-      },
-      {
-        trivia: "A very useful tool for users during development and debugging for printing content to the debugger is:",
-        answers: ["Javascript", "terminal/bash", "for loops", "console log"],
-        correct:"console log"
-      },
-]
+      quizQuestion: "String Values must be enclosed within ____ when being assigned to variables.",
+      answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+      correct:"quotes"
+    },
+    {
+      quizQuestion: "The condition in an if/else statement is enclosed with ____.",
+      answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+      correct:"curly brackets"
+    },
+    {
+      quizQuestion: "Commonly used data types DO NOT include:",
+      answers: ["strings", "booleans", "alerts", "numbers"],
+      correct:"alerts"
+    },
+    {
+      quizQuestion: "A very useful tool for users during development and debugging for printing content to the debugger is:",
+      answers: ["Javascript", "terminal/bash", "for loops", "console log"],
+      correct:"console log"
+    },
+    
+  ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,81 @@
-var highscoreScreen = document.querySelector("#highscore")
-var timerCountdown = document.querySelector("#time");
-var startQuizBtn = document.querySelector("#start-quiz-btn")
-var quizScreen = document.querySelector("#quiz-section");
-var scoreDisplay = document.querySelector("#highscore-display")
 
-function pageLoad() {
-    highscoreScreen.setAttribute("class", "hide");
-    quizScreen.setAttribute("class", "hide");
-    scoreDisplay.setAttribute("class", "hide");
-  
-    if (startQuiz == true){
-      return;
-    }
-  
-  }
-
-function startQuiz() {
-    titleScreen.setAttribute("class", "hide");
-    highscoreScreen.setAttribute("class", "hide");
-    scoreDisplay.setAttribute("class", "hide");
-   
-    quizScreen.setAttribute("class", "show");
-  
-    timer = setInterval(countdown, 1000);
-  
-    var time = 75;
-    timerCountdown.textContent = time;
-  
-    getQuestion();
-  }
-
-  var currentQuestionIndex = 0;
+var currentQuestionIndex = 0;
 var time = 75;
 var timer;
+
+
+
+
+
+var timerCountdown = document.querySelector("#time");
+var startQuizBtn = document.querySelector("#start-quiz-btn");
+var initialBtn = document.querySelector("#initials-submit-button");
+var titleScreen = document.querySelector("#start-quiz");
+var quizScreen = document.querySelector("#quiz-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackKey = document.querySelector("#key");
+var answersEl = document.querySelector("#answers");
+var highscoreScreen = document.querySelector("#highscore")
+var scoreDisplay = document.querySelector("#highscore-display")
+var hideHighScoresLink = document.querySelector("#view-highscores")
+var hideTime = document.querySelector(".time")
+
+function pageLoad() {
+  highscoreScreen.setAttribute("class", "hide");
+  quizScreen.setAttribute("class", "hide");
+  scoreDisplay.setAttribute("class", "hide");
+
+  if (startQuiz == true){
+    return;
+  }
+
+}
+
+function startQuiz() {
+  titleScreen.setAttribute("class", "hide");
+  highscoreScreen.setAttribute("class", "hide");
+  scoreDisplay.setAttribute("class", "hide");
+
+  quizScreen.setAttribute("class", "show");
+
+  timer = setInterval(countdown, 1000);
+
+  var time = 75;
+  timerCountdown.textContent = time;
+
+  getQuestion();
+}
+
+function countdown() {
+  time--;
+  timerCountdown.textContent = time;
+
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  console.log(currentQuestion)
+
+  var titleEl = document.getElementById("quiz-question");
+  titleEl.textContent = currentQuestion.quizQuestion;
+
+  answersEl.innerHTML = "";
+
+  currentQuestion.answers.forEach(function(answer, i) {
+  
+    var answerNode = document.createElement("button");
+    answerNode.setAttribute("class", "btn");
+    answerNode.setAttribute("value", answer)
+
+    answerNode.textContent = i +1 + ". " + answer;
+    answerNode.onclick = questionClick;
+
+    answersEl.appendChild(answerNode);
+  });
+   
+  
+};
+
