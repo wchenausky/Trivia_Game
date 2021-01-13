c8b038d4d89cf4bfc90dfdf969174e64c3486eb0
William Cheausky <williamchenausky@yahoo.com>
2021-01-13 12:42:27 -0500

    finsih up functions to make game work

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var timer;
 
 
 
-
+//Global Variables
 
 var timerCountdown = document.querySelector("#time");
 var startQuizBtn = document.querySelector("#start-quiz-btn");
@@ -79,3 +79,124 @@ function getQuestion() {
   
 };
 
+
+function questionClick() {
+    
+    if (this.value !== questions[currentQuestionIndex].correct) {
+      
+      time -= 10;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+      timerCountdown.textContent = time;
+  
+      feedbackKey.textContent = "Wrong!";
+    } if (this.value == questions[currentQuestionIndex].correct) {
+      time += 10;
+
+      if (time < 0) {
+        time = 0;
+      }
+  
+      timerCountdown.textContent = time;
+      feedbackKey.textContent = "Correct!";
+    };
+  
+    feedbackKey.setAttribute("class", "key");
+    setTimeout(function() {
+      feedbackKey.setAttribute("class", "key hide");
+    }, 1000);
+  
+    currentQuestionIndex++;
+  
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+  
+  
+  function quizEnd () {
+    clearInterval(timer);
+  
+    highscoreScreen.setAttribute("class", "show");
+   
+    var finalScore = document.querySelector("#final-score");
+    finalScore.textContent = time;
+  
+    quizScreen.setAttribute("class", "hide");
+  }
+  // function to save highscore 
+function saveHighscore () {
+  // get value of input box
+  var initials = initialsEl.value
+
+  //make sure value wasn't empty 
+  if (initials !== "") {
+    // get saved scores from localStorage, or if not any set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+   // format new score object for current user 
+   var newScore = {
+     score:time, 
+     initials: initials
+   };
+
+   // save to localStorage 
+   highscores.push(newScore);
+   window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+   
+  }
+  printHighscores();
+}
+
+function checkForEnter(event) {
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+function printHighscores() {
+  titleScreen.setAttribute("class", "hide");
+  scoreDisplay.setAttribute("class", "show");
+  highscoreScreen.setAttribute("class", "hide");
+  hideHighScoresLink.setAttribute("class", "hide");
+  hideTime.setAttribute("class", "hide");
+
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function(score) {
+
+    var liTag = document.createElement("li");
+    liTag.textContent = score.initials + " - " + score.score;
+
+    var olEl = document.getElementById("display-scores");
+    olEl.appendChild(liTag);
+  });
+}
+
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+
+document.getElementById("clear-btn").onclick = clearHighscores;
+
+
+
+initialBtn.onclick = saveHighscore;
+
+startQuizBtn.onclick = startQuiz;
+
+initials.onkeyup = checkForEnter;
+
+
+pageLoad();
